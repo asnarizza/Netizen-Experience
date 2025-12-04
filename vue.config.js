2a5a51c289c105ee79fcbd25c87ef37c54fd3500
rnahar <rnahar@nextlabs.com>
2025-12-04 23:51:39 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+// vue.config.js
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Netizen-Experience/'
+    : '/',
   transpileDependencies: true
-})
+};
